bc9101a78d12fa7014c3f86e33f1ccd85c00e291
Dmitriy Lyalyuev <dmitriy@lyalyuev.info>
2013-04-16 10:42:32 +0300

    Added encfs

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,9 +9,11 @@ sudo sh -c 'echo "deb http://apt.insynchq.com/ubuntu $(lsb_release -sc) non-free
 sudo apt-get update
 sudo apt-get install -y mc git git-core xbindkeys xsel gitg diffuse hamster-indicator hamster-applet hotot curl \
   xclip scrot mpd mpc sonata rsnapshot nautilus-dropbox lxc grc htop \
-  chromium-browser sublime-text-dev oracle-java7-installer insync-beta-ubuntu
+  chromium-browser sublime-text-dev oracle-java7-installer insync-beta-ubuntu \
+  encfs
 
 if [ "`dpkg -l skype | grep ii | cut '-d ' -f1`" != "ii" ]; then
     wget -O skype.deb http://www.skype.com/go/getskype-linux-beta-ubuntu-64
     dpkg -i skype.deb
+    rm -f ./skype.deb
 fi
