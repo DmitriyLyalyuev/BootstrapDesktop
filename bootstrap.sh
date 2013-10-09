7b0f2fadf6f25cdc7bcd7e2c0ffd9bc524be2dd4
Dmitriy Lyalyuev <dmitriy@lyalyuev.info>
2013-10-09 19:06:39 +0300

    Fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-sudo add-apt-repository -y ppa:a-v-shkop/chromium-dev
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-2
 sudo add-apt-repository -y ppa:webupd8team/java
-wget -O - https://d2t3ff60b2tol4.cloudfront.net/services@insynchq.com.gpg.key | sudo apt-key add -
-sudo sh -c 'echo "deb http://apt.insynchq.com/ubuntu $(lsb_release -sc) non-free" >> /etc/apt/sources.list.d/insync.list'
+sudo add-apt-repository -y ppa:git-core/ppa
 
 sudo apt-get update
 sudo apt-get install -y mc git git-core xbindkeys xsel gitg diffuse hamster-indicator hamster-applet hotot curl \
-  xclip scrot mpd mpc sonata rsnapshot nautilus-dropbox lxc grc htop \
-  chromium-browser sublime-text-dev oracle-java7-installer insync-beta-ubuntu \
-  encfs jxplorer openssh-server
+  xclip scrot mpd mpc sonata rsnapshot lxc grc htop \
+  sublime-text oracle-java7-installer encfs jxplorer openssh-server \
+  nautilus-dropbox
 
 if [ "`dpkg -l skype | grep ii | cut '-d ' -f1`" != "ii" ]; then
     wget -O skype.deb http://www.skype.com/go/getskype-linux-beta-ubuntu-64
