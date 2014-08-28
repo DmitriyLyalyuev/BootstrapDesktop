e5f41280634f90ccb926a7fe2b997cd93ab6480a
Dmitriy Lyalyuev <dmitriy@lyalyuev.info>
2014-08-28 11:42:16 +0300

    Added some soft

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,10 @@ sudo apt-get update
 sudo apt-get install -y mc git git-core xbindkeys xsel gitg diffuse hamster-indicator hamster-applet hotot curl \
   xclip scrot grc htop \
   sublime-text oracle-java7-installer encfs jxplorer openssh-server \
-  nautilus-dropbox keepassx libasound2-plugins:i386 sni-qt:i386
+  nautilus-dropbox keepassx libasound2-plugins:i386 sni-qt:i386 \
+  pidgin-libnotify pidgin-plugin-pack pidgin-extprefs pidgin-otr \
+  build-essential libcurl4-openssl-dev libmysqlclient-dev libreadline-dev libreadline6-dev libssl-dev libxml2-dev \
+  libxslt1-dev zlib1g-dev libmagickwand-dev
+
 
 sudo add-apt-repository ppa:gencfsm && sudo apt-get update && sudo apt-get -y install gnome-encfs-manager
