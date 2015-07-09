8863655149ca0fd273585703d8f2bb464fd3cc82
Dmitriy Lyalyuev <dmitriy@lyalyuev.info>
2015-07-09 19:09:44 +0300

    Add Clementine

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,7 +15,7 @@ sudo apt-get install -y build-essential libcurl4-openssl-dev libmysqlclient-dev 
   nautilus-dropbox keepassx libasound2-plugins:i386 sni-qt:i386 \
   pidgin-libnotify pidgin-plugin-pack pidgin-extprefs pidgin-otr \
   screen reptyr gnome-encfs-manager virtualbox owncloud-client wine \
-  python-pip sshpass
+  python-pip sshpass clementine
 
 # Ansible dependencies
 sudo pip install paramiko PyYAML jinja2 httplib2
