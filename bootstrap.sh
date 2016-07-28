9b5b50555f8caa49bac03d7179abbb9a117dab29
Dmitriy Lyalyuev <dmitriy@lyalyuev.info>
2016-07-28 17:09:15 +0300

    Remove natural scrolling

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,6 @@ sudo add-apt-repository -y ppa:webupd8team/java
 sudo add-apt-repository -y ppa:git-core/ppa
 sudo add-apt-repository -y ppa:gencfsm
 sudo add-apt-repository -y ppa:pmjdebruijn/darktable-release
-sudo add-apt-repository ppa:zedtux/naturalscrolling
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_14.04/ /' >> /etc/apt/sources.list.d/owncloud-client.list"
 
 
@@ -24,7 +23,7 @@ sudo apt-get install -y build-essential libcurl4-openssl-dev libmysqlclient-dev 
   pidgin-libnotify pidgin-plugin-pack pidgin-extprefs pidgin-otr \
   screen reptyr gnome-encfs-manager virtualbox owncloud-client wine \
   python-pip sshpass clementine libyaml-dev darktable python-dev apg \
-  virtaulenv virtaulenvwrapper terminator acpi naturalscrolling
+  virtualenv virtualenvwrapper terminator acpi
 
 # Ansible dependencies
 sudo pip install paramiko PyYAML jinja2 httplib2
