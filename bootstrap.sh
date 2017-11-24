6e4ea0710499b852f9e61150224e99139e258228
Dmitriy Lyalyuev <dmitriy@lyalyuev.info>
2017-11-24 10:44:53 +0200

    Add clipit

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,7 +14,7 @@ sudo apt install -y build-essential libcurl4-openssl-dev libmysqlclient-dev pyth
   mc git git-core tig xbindkeys xsel diffuse curl xclip grc htop sublime-text oracle-java8-installer \
   encfs openssh-server keepassx screen gnome-encfs-manager python-pip sshpass clementine libyaml-dev python-dev apg \
   virtualenv virtualenvwrapper terminator acpi zenity texlive-full libyajl-dev libinput-tools xdotool \
-  cvs
+  cvs clipit
 
 # Install docker
 curl -sSL https://get.docker.com/ | sh
